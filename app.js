c326d12c132558dd402013a3569bc205e97ccfa4
nomi511 <thebespokeone@gmail.com>
2022-11-24 23:07:49 +0500

    api request limit added

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const users = require('./routes/users')
 const campaigns = require('./routes/campaign')
 const activeCampaigns = require('./routes/activeCampaign')
 require('dotenv').config()
-
+const rateLimit = require('express-rate-limit')
 const app = express()
 
 
@@ -14,6 +14,16 @@ app.use(express.json())
 // app.get('/api/v1/', (req,res)=>{
 //     res.send("wow requested")
 // })
+
+
+// api requests limit
+app.use(rateLimit({
+        windowMs: 1 * 60 * 60 * 1000,
+        max: 50,
+        message: {success: false, message: 'Too many requests please try again after One Hour.'},
+        headers: true
+    })
+)
 
 // users route
 app.use('/api/v1/users', users)
